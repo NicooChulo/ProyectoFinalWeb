49bef7300e2e404e5a3e2958af796ced7ec1186f
Nico Reinaldo <31456300+NicooChulo@users.noreply.github.com>
2022-02-15 19:02:01 -0300

    Data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO empleados (`id`, `apellido`, `dni`, `nombre`) VALUES ('1', 'betancur', '30677736', 'damian');
+INSERT INTO empleados (`id`, `apellido`, `dni`, `nombre`) VALUES ('2', 'betancur', '4000000', 'damian');
+INSERT INTO empleados (`id`, `apellido`, `dni`, `nombre`) VALUES ('3', 'betancur', '3067776', 'damian');
+INSERT INTO empleados (`id`, `apellido`, `dni`, `nombre`) VALUES ('4', 'betancur', '3557736', 'damian');
+INSERT INTO empleados (`id`, `apellido`, `dni`, `nombre`) VALUES ('5', 'betancur', '3557736', 'damian');
+
+INSERT INTO estado_campos (`id`, `descripcion`) VALUES ('1', 'creado');
+INSERT INTO estado_campos (`id`, `descripcion`) VALUES ('2', 'trabajado');
+INSERT INTO estado_campos (`id`, `descripcion`) VALUES ('3', 'parcialmente trabajado');
+INSERT INTO estado_campos (`id`, `descripcion`) VALUES ('4', 'desuso');
+
+INSERT INTO campos (`id`, `numero_campo`, `estado_de_campo_id`) VALUES ('1', '0003', '1');
+
+
+
