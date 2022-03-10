75c9dceb7c0e0213fb3f747014e20ce071f8fa1b
Nico Reinaldo <31456300+NicooChulo@users.noreply.github.com>
2022-03-10 09:36:23 -0300

    proyect fixed

--- src/main/java/com/proyectofinalweb/proyectofinalweb/controller/CampoController.java
+++ src/main/java/com/proyectofinalweb/proyectofinalweb/controller/CampoController.java
@@ -1,10 +1,12 @@
 package com.proyectofinalweb.proyectofinalweb.controller;
 
 import com.proyectofinalweb.proyectofinalweb.dto.CampoDTO;
+import com.proyectofinalweb.proyectofinalweb.dto.EmpleadoDTO;
 import com.proyectofinalweb.proyectofinalweb.service.crud.ICampoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,8 +17,6 @@ public class CampoController {
 
     private static final Logger logger = LoggerFactory.getLogger(CampoController.class);
     public static final String RESOURCE = "/api/v1/campos";
-
-    public static final String DNI = "/{empleado_dni}";
 
     public static final String DNI = "/{campo_id}";
 
@@ -30,33 +30,13 @@ public class CampoController {
 
   @RequestMapping(value = DNI, method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity<CampoDTO> getOneCampo(@PathVariable("numero_campo") String numeroCampo) {
-        logger.info("Buscar un empleado por dni {}", numeroCampo);
-        CampoDTO campo = this.campoService.bucarCampoPorNumero(numeroCampo);
-        if (campo == null) {
-            return new ResponseEntity<CampoDTO>(HttpStatus.NOT_FOUND);
-          
-   /* @RequestMapping(value = DNI, method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity<CampoDTO> getOneCampo(@PathVariable("campo_id") String campoID) {
-        logger.info("Buscar un campo por id {}", campoID);
-        EmpleadoDTO empleado = this.campoService.buscarEmpleadoPorDNI(campoID);
-        if (empleado == null) {
-            return new ResponseEntity<EmpleadoDTO>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<EmpleadoDTO>(empleado, HttpStatus.OK);
-    }
-
-    public ResponseEntity<Collection<EmpleadoDTO>> getEmpleados(){
-        logger.info("Get All Hotels");
-        Collection<EmpleadoDTO> empleados = new ArrayList<EmpleadoDTO>();
-        empleados.addAll(this.campoService.listarTodos());
-        if (empleados.isEmpty()){
-            return new ResponseEntity<Collection<EmpleadoDTO>>(HttpStatus.NOT_FOUND);
-        }else{
-            return new ResponseEntity<Collection<EmpleadoDTO>>(empleados, HttpStatus.OK);
-        }
-    }
-*/
-
+      logger.info("Buscar un empleado por dni {}", numeroCampo);
+      CampoDTO campo = this.campoService.buscarCampoPorNumero(numeroCampo);
+      if (campo == null) {
+          return new ResponseEntity<CampoDTO>(HttpStatus.NOT_FOUND);
+      }
+      return new ResponseEntity<CampoDTO>(campo, HttpStatus.OK);
+  }
     @RequestMapping(method = RequestMethod.POST, consumes="application/json")
     @ResponseBody
     public ResponseEntity<CampoDTO> create(@RequestBody CampoDTO nuevoCampo) {
@@ -64,25 +44,11 @@ public class CampoController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
         headers.add("Responded", "EmpleadoController");
-        headers.add("Responded", "CampoController");
-
         try {
             if (campoService.buscarCampoPorNumero(nuevoCampo.getNumeroCampo())==null){
 
                 campoRespuesta = campoService.registrar(nuevoCampo);
 
-            }
-
-        } catch(Exception ex) {
-
-            System.out.println("Employee not found" + ex.getMessage());
-
-        headers.add("Responded", "CampoController");
-
-        try {
-            if (campoService.buscarCampoPorNumero(nuevoCampo.getNumeroCampo())==null){
-
-                campoRespuesta = campoService.registrar(nuevoCampo);
             }
 
         } catch(Exception ex) {
--- src/main/java/com/proyectofinalweb/proyectofinalweb/service/crud/ICampoService.java
+++ src/main/java/com/proyectofinalweb/proyectofinalweb/service/crud/ICampoService.java
@@ -3,16 +3,9 @@ package com.proyectofinalweb.proyectofinalweb.service.crud;
 
 import com.proyectofinalweb.proyectofinalweb.dto.CampoDTO;
 
-<<<<<<< Updated upstream
-public interface ICampoService extends ICRUD<CampoDTO>{
-
-    CampoDTO buscarCampoPorNumero(String numeroCampo);
-
-=======
 
 public interface ICampoService extends ICRUD<CampoDTO>{
 
     CampoDTO buscarCampoPorNumero(String numeroCampo);
 
->>>>>>> Stashed changes
 }
